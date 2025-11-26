69007483f9a7aab4266ed8782b2d54be16178cd7
James <james.ernest.hanson@gmail.com>
2025-11-26 13:35:37 -0600

    Modified Theorem 7

--- JunkTheorems.lean
+++ JunkTheorems.lean
@@ -206,7 +206,7 @@ def QR_mod_eq := Quotient QR_setoid
 Now let `r` be the equivalence class of the unique proof of quadratic reciprocity under equality.
 -/
 def r : QR_mod_eq :=
-  (Quot.mk (· = ·) (by unfold QR; grind [jacobiSym.quadratic_reciprocity]))
+  Quot.mk (· = ·) (by unfold QR; grind [jacobiSym.quadratic_reciprocity])
 /-!
 Use the axiom of choice to pick an element `q` of `QR/=`. (Choice needs us to give it `r` so that
 it knows the the type `QR/=` is nonempty.)
@@ -222,37 +222,44 @@ the equivalence relation of equality, so it lifts to a function `f` from `QR/=` 
 -/
 def f : QR_mod_eq → ℕ := Quot.lift (fun _ ↦ 1) (by grind)
 /-!
-Recall that `Fin n` is the type of natural numbers less than `n` (i.e., `Fin n` is like the set
-`{0,1,...,n-1}`). Let `a` be the constant zero function on `Fin 1`, `b` be the constant zero
-function on `Fin (f r)`, and `c` be the constant zero function on `Fin (f q)`.
+Use the fact that `q` equals `r` to prove that `f q = 1`.
 -/
-def a : Fin 1 → ℕ := fun _ ↦ 0
-
-def b : Fin (f r) → ℕ := fun _ ↦ 0
-
-def c : Fin (f q) → ℕ := fun _ ↦ 0
+lemma f_q_eq_one : f q = 1 := by rw [q_eq_r]; unfold f r; grind
 /-!
-Now, since `f q = 1` and `f r = 1`, `Fin 1`, `Fin (f q)`, and `Fin (f r)` should all be the same
-type, and, moreover, `a`, `b`, and `c` should all be the same zero function. In particular, we can
-easily prove the following:
-**Theorem 7.** `a = b + c` and `b + c = c + b`.
+Recall that `Fin n` is the type of natural numbers less than `n` (i.e., `Fin n` is like the set
+`{0,1,...,n-1}`). Let `a` be `0` in the type `Fin (f q)`, `b` be `0` in the type `Fin (f r)`, and
+`c` be `0` in the type `Fin 1`. For `a` and `b` we need to provide proofs that `0` is less than
+`f q` and `f r`, respectively. (Fortunately, Lean is smart enough to figure out that `0 < 1` on its
+own for `c`.)
 -/
-theorem a_eq_b_plus_c_eq_c_plus_b : a = b + c
-                                  ∧ b + c = c + b := by
+def a : Fin (f q) := ⟨0, by rw [f_q_eq_one]; simp⟩
+
+def b : Fin (f r) := ⟨0, by unfold f r; grind⟩
+
+def c : Fin 1 := 0
+/-!
+Now, since `f q = 1` and `f r = 1`, `Fin (f q)`, `Fin (f r)`, and `Fin 1` should all be the same
+type, and, moreover, `a`, `b`, and `c` should all be the same thing.
+
+Indeed we can almost prove this:
+
+**Theorem 7.** `a` is equal to `b`, and `b` is equal to `c`.
+-/
+theorem a_eq_b_eq_c : a = b ∧ b = c := by
   constructor
   · rfl
   · rfl
 /-!
-But now there's an issue. If we ask Lean, it will tell us that `a` and `c + b` don't have the same
-type:
+But now there's an issue. If we ask Lean, it will tell us that `a` and `c` don't have the same type:
 -/
-#check_failure a = c + b
+#check_failure a = c
 /-!
-This means that it doesn't even make sense to say that `a` is equal to `c + b`.
+This means that it doesn't even make sense to say that `a` is equal to `c`.
 
-However, it is easy to show that `a` and `c + b` are heterogeneously equal, by appealing to Theorem
-7 and the basic mathematical fact that addition is commutative:
+However, it is easy to show that `a` and `c` are heterogeneously equal, since equality is
+transitive, of course:
 -/
-theorem a_heq_c_plus_b : ⟨Fin 1 → ℕ, a⟩ = (⟨Fin (f q) → ℕ, c + b⟩ : Σ X : Type, X) := by
-  rw [add_comm]
-  grind [a_eq_b_plus_c_eq_c_plus_b]
+theorem a_heq_c : ⟨Fin (f q), a⟩ = (⟨Fin 1, c⟩ : Σ' X : Type, X) := by
+  have h0 : ⟨Fin (f q), a⟩ = (⟨Fin (f r), b⟩ : Σ' X : Type, X) := rfl
+  have h1 : ⟨Fin (f r), b⟩ = (⟨Fin 1, c⟩ : Σ' X : Type, X) := rfl
+  simp [h0,h1]
